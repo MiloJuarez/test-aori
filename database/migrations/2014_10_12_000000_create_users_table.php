a7802a279f7b48143f17146372626664d44b55b5
Milo Juarez <ermilo98go@gmail.com>
2022-05-06 13:02:14 -0500

    [UPDATE] Set users fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('edad');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
