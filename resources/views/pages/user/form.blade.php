ae260550c231e7ff612784524f8dd77042af3765
Milo Juarez <ermilo98go@gmail.com>
2022-05-12 20:31:49 -0500

    [UPDATE] Form labels & text color

--- resources/views/pages/user/form.blade.php
+++ resources/views/pages/user/form.blade.php
@@ -1,23 +1,23 @@
-<div>
+<div class="text-white">
     @if (isset($user))
         <input type="hidden" name="identifier" value="{{ $user->id }}">
     @endif
     @csrf
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" name="name" id="name" class="form-control" placeholder="User name"
+        <input type="text" name="name" id="name" class="form-control" placeholder="Eje. Jhon"
             value="{{ $user->nombre ?? '' }}">
     </div>
 
     <div class="mb-3">
-        <label for="lastname" class="form-label">Name</label>
-        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="User lastname"
+        <label for="lastname" class="form-label">Lastname</label>
+        <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Eje. Connor"
             value="{{ $user->apellido ?? '' }}">
     </div>
 
     <div class="mb-3">
-        <label for="age" class="form-label">Name</label>
-        <input type="number" name="age" id="age" class="form-control" placeholder="User age"
+        <label for="age" class="form-label">Age</label>
+        <input type="number" name="age" id="age" class="form-control" placeholder="56"
             value="{{ $user->edad ?? '' }}">
     </div>
 </div>
